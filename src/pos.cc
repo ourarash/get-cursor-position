bcba6b3ae0a47a074bd0db21c6b98cd0b085e0db
Ari <ourarash@gmail.com>
2019-07-18 20:27:39 -0700

    Update pos.cc

--- src/pos.cc
+++ src/pos.cc
@@ -258,7 +258,7 @@ void Method(const v8::FunctionCallbackInfo<Value>& args) {
   	args.GetReturnValue().Set(pos);
 }
 
-void Init(Handle<Object> exports) {
+void Init(HANDLE<Object> exports) {
   Isolate* isolate = Isolate::GetCurrent();
   exports->Set(String::NewFromUtf8(isolate, "sync"),
       FunctionTemplate::New(isolate, Method)->GetFunction());
